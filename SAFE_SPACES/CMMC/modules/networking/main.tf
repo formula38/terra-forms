a83b9da0e2926b77f221e9d280dc689138f48ed3
formula38 <louismconley@gmail.com>
2025-03-29 22:08:51 -0700

    + fully validated CMMC compliant infra

--- SAFE_SPACES/CMMC/modules/networking/main.tf
+++ SAFE_SPACES/CMMC/modules/networking/main.tf
@@ -88,28 +88,26 @@ resource "aws_security_group" "main_sg" {
   description = "Main security group"
   vpc_id      = aws_vpc.main.id
 
-  ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = [var.trusted_ip_range]
+  dynamic "ingress" {
+    for_each = var.security_group_ingress_rules
+    content {
+      description = ingress.value.description
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
   }
 
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.trusted_ip_range]
-  }
-
-  egress {
-    description = "All outbound"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "egress" {
+    for_each = var.security_group_egress_rules
+    content {
+      description = egress.value.description
+      from_port   = egress.value.from_port
+      to_port     = egress.value.to_port
+      protocol    = egress.value.protocol
+      cidr_blocks = egress.value.cidr_blocks
+    }
   }
 
   tags = merge(
@@ -120,3 +118,4 @@ resource "aws_security_group" "main_sg" {
     var.common_tags
   )
 }
+
--- SAFE_SPACES/CMMC/modules/networking/variables.tf
+++ SAFE_SPACES/CMMC/modules/networking/variables.tf
@@ -42,3 +42,51 @@ variable "common_tags" {
   description = "Common tags to apply to all resources"
   type        = map(string)
 }
+
+variable "security_group_ingress_rules" {
+  description = "List of ingress rules for the security group"
+  type = list(object({
+    description = string
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = [
+    {
+      description = "HTTPS"
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["203.0.113.0/24"]
+    },
+    {
+      description = "SSH"
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["203.0.113.0/24"]
+    }
+  ]
+}
+
+variable "security_group_egress_rules" {
+  description = "List of egress rules"
+  type = list(object({
+    description = string
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = [
+    {
+      description = "All outbound"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
+
